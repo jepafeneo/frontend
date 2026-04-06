f5f676e68e3ade6c44d1309887cbcc66e7278bad
Jean Paul Ferreira <jeanpaul@jepafe.net>
2026-04-06 15:20:41 -0300

    Componente App

---
+++ clase-01/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  const name = "Jean";
+
+  return (
+    <>
+      <h1>Hola {name}</h1>
+      <p>Bienvenidos a React</p>
+    </>
+  );
+}
+
+export default App;
