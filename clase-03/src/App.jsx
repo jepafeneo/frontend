2f37cf7f5d7d162debebc5d73269ac8e7fb462c4
Jean Paul Ferreira <jeanpaul@jepafe.net>
2026-04-08 16:53:19 -0300

    Add Product

--- clase-03/src/App.jsx
+++ clase-03/src/App.jsx
@@ -5,6 +5,7 @@ import ProductList from "./components/ProductList";
 import Subtitle from "./components/Subtitle";
 import Title from "./components/Title";
 import { useState } from "react";
+import Saludar from "./components/Saludar";
 
 function App() {
   const [products, setProducts] = useState([
@@ -14,19 +15,15 @@ function App() {
     { id: 4, name: "Monitor", price: 300, category: "Monitors" },
   ]);
 
-  const [newProductName, setNewProductName] = useState("Juan");
-
-  // const products = [
-  //   { id: 1, name: "Laptop", price: 1000, category: "Electronics" },
-  //   { id: 2, name: "Mouse", price: 50, category: "Accessories" },
-  //   { id: 3, name: "Keyboard", price: 80, category: "Electronics" },
-  //   { id: 4, name: "Monitor", price: 300, category: "Monitors" },
-  // ];
+  const [newProductName, setNewProductName] = useState("");
 
   const addProduct = () => {
+    // null, undefined, 0, ''
+    if (!newProductName.trim()) return;
+
     const newProduct = {
-      id: Date.now(),
-      name: "Monitor Asus",
+      id: Date.now(), // Dice que es una función impura
+      name: newProductName,
       price: 350,
       category: "Monitors",
     };
@@ -35,15 +32,10 @@ function App() {
 
     // products.push(newProduct);
     setProducts([...products, newProduct]);
+    setNewProductName("");
 
     console.log(products);
   };
-
-  // const categories = [
-  //   { id: 1, name: "Tecnología" },
-  //   { id: 2, name: "Audio" },
-  //   { id: 3, name: "Accesorios" },
-  // ];
 
   const [categories, setCategories] = useState([
     { id: 1, name: "Tecnología" },
@@ -52,20 +44,28 @@ function App() {
   ]);
 
   const addCategory = () => {
-    const newCategory = { id: Date.now(), name: "Gaming" };
+    const newCategory = { id: Date.now(), name: newCategoryName };
     setCategories([...categories, newCategory]);
+    setNewCategoryName("");
   };
+
+  const [newCategoryName, setNewCategoryName] = useState("");
 
   return (
     <>
+      <Saludar />
+
       <Title title="Listado de productos" />
 
       <ProductList products={products} />
+
+      <p>{newProductName}</p>
 
       <input
         type="text"
         value={newProductName}
         onChange={(event) => setNewProductName(event.target.value)}
+        onKeyDown={(e) => e.key == "Enter" && addProduct()}
       />
 
       <button onClick={addProduct}>Agregar producto</button>
@@ -76,7 +76,13 @@ function App() {
 
       <CategoryList categories={categories} />
 
-      
+      <p>{newCategoryName}</p>
+
+      <input
+        type="text"
+        value={newCategoryName}
+        onChange={(event) => setNewCategoryName(event.target.value)}
+      />
 
       <button onClick={addCategory}>Agregar categoría</button>
 
