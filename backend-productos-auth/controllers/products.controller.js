29a2486b1ec1686d925909ad8d96aabb55bb17d9
Jean Paul Ferreira <jeanpaul@jepafe.net>
2026-04-16 14:07:53 -0300

    Pausa en getProducts

--- backend-productos-auth/controllers/products.controller.js
+++ backend-productos-auth/controllers/products.controller.js
@@ -2,6 +2,8 @@ import Product from "../models/Product.js";
 
 export const getProducts = async (req, res) => {
   try {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+
     const products = await Product.find();
     res.json(products);
   } catch (error) {
