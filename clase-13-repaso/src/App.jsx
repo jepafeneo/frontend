89487d6de137f61c734f8d7bf93e93e90e5f1eff
Jean Paul Ferreira <jeanpaul@jepafe.net>
2026-04-22 15:52:37 -0300

    handleSubmit

--- clase-13-repaso/src/App.jsx
+++ clase-13-repaso/src/App.jsx
@@ -9,36 +9,56 @@ function App() {
   const handleChangeCorreo = (event) => setCorreo(event.target.value);
   const handleChangeMensaje = (event) => setMensaje(event.target.value);
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const consulta = {
+      nombre,
+      correo: correo,
+      mensaje: mensaje,
+    };
+
+    console.log(consulta);
+  };
+
   return (
     <>
-      <input
-        type="text"
-        name="nombre"
-        id="nombre"
-        placeholder="Escriba su nombre"
-        value={nombre}
-        onChange={handleChangeNombre}
-      />
-      <p>{nombre}</p>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="nombre"
+          id="nombre"
+          placeholder="Escriba su nombre"
+          value={nombre}
+          onChange={handleChangeNombre}
+        />
+        <p>{nombre}</p>
 
-      <input
-        type="correo"
-        id="correo"
-        placeholder="Escribe tu correo"
-        value={correo}
-        onChange={handleChangeCorreo}
-      />
-      <p> {correo} </p>
+        <input
+          type="correo"
+          id="correo"
+          placeholder="Escribe tu correo"
+          value={correo}
+          onChange={handleChangeCorreo}
+        />
+        <p> {correo} </p>
 
-      <textarea
-        type="text"
-        name="mensaje"
-        id="mensaje"
-        placeholder="Mensaje"
-        value={mensaje}
-        onChange={handleChangeMensaje}
-      />
-      <p>{mensaje}</p>
+        <textarea
+          type="text"
+          name="mensaje"
+          id="mensaje"
+          placeholder="Mensaje"
+          value={mensaje}
+          onChange={handleChangeMensaje}
+        />
+        <p>{mensaje}</p>
+
+        <button type="submit">Enviar 1</button>
+
+        {/* <button type="button" onClick={() => console.log("clic")}>
+          Enviar 2
+        </button> */}
+      </form>
     </>
   );
 }
