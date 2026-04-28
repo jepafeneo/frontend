1487436768a1b059b6723e763a9581a404ca3aab
Jean Paul Ferreira <jeanpaul@jepafe.net>
2026-04-28 15:48:36 -0300

    handleSubmit

--- clase-17/src/App.jsx
+++ clase-17/src/App.jsx
@@ -1,6 +1,35 @@
+import { useState } from "react";
 import "./App.css";
 
+const initialForm = {
+  title: "",
+  status: "pending",
+};
+
 function App() {
+  const [form, setForm] = useState(initialForm);
+  const [tasks, setTasks] = useState([]);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setForm({ ...form, [name]: value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const task = {
+      id: crypto.randomUUID(),
+      title: form.title,
+      status: form.status,
+    };
+
+    // tasks.push(task) // No usar con estados
+    setTasks([...tasks, task]);
+    setForm(initialForm);
+  };
+
   return (
     <main className="container">
       <h1>Gestor de tareas</h1>
@@ -8,15 +37,26 @@ function App() {
       <section className="form-section">
         <h2>Nueva tarea</h2>
 
-        <form className="task-form">
+        <form onSubmit={handleSubmit} className="task-form">
           <div className="form-group">
             <label htmlFor="title">Tarea: </label>
-            <input type="text" name="title" id="title" />
+            <input
+              type="text"
+              name="title"
+              id="title"
+              value={form.title}
+              onChange={handleChange}
+            />
           </div>
 
           <div className="form-group">
             <label htmlFor="status">Estado: </label>
-            <select name="status" id="status">
+            <select
+              name="status"
+              id="status"
+              value={form.status}
+              onChange={handleChange}
+            >
               <option value="pending">Pendiente</option>
               <option value="done">Terminada</option>
             </select>
