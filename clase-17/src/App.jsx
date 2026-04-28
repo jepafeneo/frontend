0f1801a0b49ed1ab855cebca788d7dc2f8916510
Jean Paul Ferreira <jeanpaul@jepafe.net>
2026-04-28 17:01:25 -0300

    Iconos

--- clase-17/src/App.jsx
+++ clase-17/src/App.jsx
@@ -9,6 +9,7 @@ const initialForm = {
 function App() {
   const [form, setForm] = useState(initialForm);
   const [tasks, setTasks] = useState([]);
+  const [error, setError] = useState("");
 
   const handleChange = (event) => {
     const { name, value } = event.target;
@@ -19,6 +20,18 @@ function App() {
   const handleSubmit = (event) => {
     event.preventDefault();
 
+    if (!form.title.trim()) {
+      setError("La tarea es obligatoria");
+      return;
+    }
+
+    if (form.title.trim().length < 3) {
+      setError("La tarea tiene que tener 3 caracteres o mas");
+      return;
+    }
+
+    setError("");
+
     const task = {
       id: crypto.randomUUID(),
       title: form.title,
@@ -28,6 +41,13 @@ function App() {
     // tasks.push(task) // No usar con estados
     setTasks([...tasks, task]);
     setForm(initialForm);
+  };
+
+  const handleDelete = (id) => {
+    // const filtered = tasks.filter((t) => t.id != id);
+    // setTasks(filtered);
+
+    setTasks(tasks.filter((t) => t.id != id));
   };
 
   return (
@@ -62,6 +82,8 @@ function App() {
             </select>
           </div>
 
+          {error && <p className="error">{error}</p>}
+
           <button type="submit">Crear tarea</button>
         </form>
       </section>
@@ -74,6 +96,27 @@ function App() {
             <article key={task.id} className="task-card">
               <h3>{task.title}</h3>
               <p>{task.status}</p>
+
+              <div className="actions">
+                <button type="button">Editar</button>
+                <button type="button" onClick={() => handleDelete(task.id)}>
+                  Eliminar
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="size-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                    />
+                  </svg>
+                </button>
+              </div>
             </article>
           ))}
         </div>
