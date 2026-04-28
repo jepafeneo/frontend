d5ecadfb32683ab9034050fe3540dabff7d38e35
Jean Paul Ferreira <jeanpaul@jepafe.net>
2026-04-28 14:36:18 -0300

    Formulario sin estilo

--- clase-17/src/App.jsx
+++ clase-17/src/App.jsx
@@ -10,9 +10,19 @@ function App() {
 
         <form className="task-form">
           <div className="form-group">
-            <label htmlFor="title">Titulo</label>
+            <label htmlFor="title">Tarea: </label>
             <input type="text" name="title" id="title" />
           </div>
+
+          <div className="form-group">
+            <label htmlFor="status">Estado: </label>
+            <select name="status" id="status">
+              <option value="pending">Pendiente</option>
+              <option value="done">Terminada</option>
+            </select>
+          </div>
+
+          <button type="submit">Crear tarea</button>
         </form>
       </section>
     </main>
