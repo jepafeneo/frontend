7c68594b5b5880037f4b529394ce4d20c86b6691
Jean Paul Ferreira <jeanpaul@jepafe.net>
2026-05-05 14:29:03 -0300

    AuthService

---
+++ clase-21/src/services/AuthService.js
@@ -0,0 +1,19 @@
+const API_URL = "http://localhost:3000/auth";
+
+export const registerUser = async (user) => {
+  const response = await fetch(`${API_URL}/register`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(user),
+  });
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.error || `Error al registrar un usuario`);
+  }
+
+  return data;
+};
