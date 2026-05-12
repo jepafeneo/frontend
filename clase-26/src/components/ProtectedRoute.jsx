7588b7c56b52ae2cbed348834516f33a265bce0f
Jean Paul Ferreira <jeanpaul@jepafe.net>
2026-05-12 16:55:00 -0300

    ProtectedRoute

--- clase-26/src/components/ProtectedRoute.jsx
+++ clase-26/src/components/ProtectedRoute.jsx
@@ -1,18 +1,38 @@
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../hooks/useAuth";
+import { useState, useEffect } from "react";
+import { getProfile } from "../services/AuthService";
 
-function ProtectedRoute({ children }) {
-  const { user, authLoading } = useAuth();
+function ProtectedRouter({ children }) {
+  const { user, authLoading, logout } = useAuth();
+  const [checking, setChecking] = useState(true);
+  const [isValid, setIsValid] = useState(false);
 
-  if (authLoading) {
+  useEffect(() => {
+    const verifyAccess = async () => {
+      try {
+        await getProfile();
+        setIsValid(true);
+      } catch {
+        logout();
+        setIsValid(false);
+      } finally {
+        setChecking(false);
+      }
+    };
+
+    verifyAccess();
+  }, [children]);
+
+  if (authLoading || checking) {
     return <p>Verificando usuario...</p>;
   }
 
-  if (!user) {
+  if (!user || !isValid) {
     return <Navigate to="/login" />;
   }
 
   return children;
 }
 
-export default ProtectedRoute;
+export default ProtectedRouter;
