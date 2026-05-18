8d66bbcfa5a361a5ea5554401245a6cff815b101
Jean Paul Ferreira <jeanpaul@jepafe.net>
2026-05-18 15:07:21 -0300

    ProductCard CSS Modules

--- clase-28/src/components/ProductCard.jsx
+++ clase-28/src/components/ProductCard.jsx
@@ -1,15 +1,16 @@
 import { Link } from "react-router-dom";
 import { useAuth } from "../hooks/useAuth";
+import styles from "./ProductCard.module.css";
 
 function ProductCard({ product, handleDelete }) {
   const { user } = useAuth();
 
   return (
-    <article className="product-card">
-      <h3>{product.name}</h3>
-      <p>$ {product.price}</p>
+    <article className={styles.card}>
+      <h3 className={styles.title}>{product.name}</h3>
+      <p className={styles.price}>$ {product.price}</p>
 
-      <div className="card-actions">
+      <div className={styles.actions}>
         <Link to={"/products/" + product._id} className="button">
           Detalle
         </Link>
